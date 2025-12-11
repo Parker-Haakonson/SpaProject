bf53bfce9cbae9e41c8365f8c4bb18109e1e9b46
Parker Haakonson <parker.haakonson@ndus.edu>
2025-12-11 17:31:43 -0600

    Login feature

---
+++ SpaProject/LogedIn.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaProject
+{
+    static class LogedIn
+    {
+        static bool IsLogedIn = false;
+
+        static public bool GetLogedIn()
+        {
+            return IsLogedIn;
+        }
+        static public void SetLogedIn(bool logedIn)
+        {
+            IsLogedIn = true;
+        }
+    }
+}
