774dbbcc3527e5f623c6797d9f42907efe76884f
extremegamer561-eng <extremegamer561@gmail.com>
2025-12-15 20:07:26 -0600

    Revert "Added functional loggout button"
    This reverts commit 9ce7a490cd749d887537448df4ea34de801bf3ed.

--- SpaProject/LogedIn.cs
+++ SpaProject/LogedIn.cs
@@ -7,52 +7,42 @@
         static string email;
         static string password;
         static bool isAdmin = false;
-        
         static public bool GetLogedIn()
         {
             return IsLogedIn;
         }
-        
         static public void SetLogedIn(bool logedIn)
         {
-            IsLogedIn = logedIn;
+            IsLogedIn = true;
         }
-        
         static public string GetEmail()
         {
             return email;
         }
-        
         static public void SetEmail(string Email)
         {
             email = Email;
         }
-        
         static public string GetPassword()
         {
             return password;
         }
-        
         static public void SetPassword(string Password)
         {
             password = Password;
         }
-        
         static public bool GetIsAdmin()
         {
             return isAdmin;
         }
-        
         static public void SetIsAdmin(bool IsAdmin)
         {
             isAdmin = IsAdmin;
         }
-        
         static public int GetId()
         {
             return id;
         }
-        
         static public void SetId(int Id)
         {
             id = Id;
