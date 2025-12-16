dd5113aebc7ae1aae8af3a9fb9c38c69394cb451
extremegamer561-eng <extremegamer561@gmail.com>
2025-12-16 16:56:48 -0600

    restrants to assigned staff & calander

--- SpaProject/Models/Service.cs
+++ SpaProject/Models/Service.cs
@@ -1,19 +1,32 @@
-﻿namespace SpaProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaProject.Models
 {
     public class Service
     {
         public int Id { get; set; }
 
         public int CustomerId { get; set; }
+        
+        [Required(ErrorMessage = "Service name is required.")]
         public string Name { get; set; }
+        
+        [Required]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please assign a staff member.")]
         public string assignedStaff { get; set; }
 
+        [Required]
         public int month { get; set; }
+        
+        [Required]
         public int year { get; set; }
+        
+        [Required]
         public int day { get; set; }
         
+        [Required]
         public int time { get; set; }
     }
 }
